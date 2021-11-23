eb1a61bf6b0ec29445864a90e8d2f2b83d17964e
liampongracz <liam.pongracz@gmail.com>
2021-11-22 22:44:35 -0500

    initial commit

---
+++ alembic/fastAPI_cmd.py
@@ -0,0 +1,10 @@
+# start app
+# uvicorn app.main:app
+
+# activate venv
+# source path/venv/bin/activate
+
+# upgrade alembic 
+# alembic upgrade head
+# autogenerate from models
+# alembic revision --autogenerate -m "update"
